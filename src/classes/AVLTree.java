0c69427cc6f19e4edb19f085bfe47ead52d10942
Felipe Fernandes <lipedm.fernandes@hotmail.com>
2021-05-07 10:29:14 -0300

    fix tabulation

--- src/classes/AVLTree.java
+++ src/classes/AVLTree.java
@@ -19,37 +19,28 @@ public class AVLTree {
             node.left  = insert(node.left, value);
         else
             node.right = insert(node.right, value);
-
         
         node.height = Math.max(height(node.left), height(node.right)) + 1;
-
         
         int balance = getBalance(node);
-
-      
-
        
         if (balance > 1 && value < node.left.value)
             return rightRotate(node);
-
        
         if (balance < -1 && value > node.right.value)
             return leftRotate(node);
-
         
         if (balance > 1 && value > node.left.value)
         {
             node.left =  leftRotate(node.left);
             return rightRotate(node);
         }
-
         
         if (balance < -1 && value < node.right.value)
         {
             node.right = rightRotate(node.right);
             return leftRotate(node);
         }
-
         
         return node;
     }
@@ -57,15 +48,12 @@ public class AVLTree {
     private Node rightRotate(Node y) {
         Node x = y.left;
         Node T2 = x.right;
-
        
         x.right = y;
         y.left = T2;
-
         
         y.height = Math.max(height(y.left), height(y.right))+1;
         x.height = Math.max(height(x.left), height(x.right))+1;
-
        
         return x;
     }
@@ -73,15 +61,12 @@ public class AVLTree {
     private Node leftRotate(Node x) {
         Node y = x.right;
         Node T2 = y.left;
-
         
         y.left = x;
         x.right = T2;
-
        
         x.height = Math.max(height(x.left), height(x.right))+1;
         y.height = Math.max(height(y.left), height(y.right))+1;
-
         
         return y;
     }
@@ -109,30 +94,24 @@ public class AVLTree {
         return current;
     }
 
-    public Node deleteNode(Node root, int value) {
-       
+    public Node deleteNode(Node root, int value) {       
         if (root == null)
             return root;
-
         
         if ( value < root.value )
             root.left = deleteNode(root.left, value);
-
        
         else if( value > root.value )
             root.right = deleteNode(root.right, value);
-
         
         else {
            
             if( (root.left == null) || (root.right == null) ) {
-
                 Node temp;
                 if (root.left != null)
                         temp = root.left;
                 else
                     temp = root.right;
-
                 
                 if(temp == null) {
                     temp = root;
@@ -143,42 +122,32 @@ public class AVLTree {
 
                 temp = null;
             }
-            else {
-               
+            else {               
                 Node temp = minValueNode(root.right);
-
                 
                 root.value = temp.value;
-
                 
                 root.right = deleteNode(root.right, temp.value);
             }
         }
-
        
         if (root == null)
             return root;
-
         
         root.height = Math.max(height(root.left), height(root.right)) + 1;
 
-
         int balance = getBalance(root);
-
        
         if (balance > 1 && getBalance(root.left) >= 0)
             return rightRotate(root);
-
         
         if (balance > 1 && getBalance(root.left) < 0) {
             root.left =  leftRotate(root.left);
             return rightRotate(root);
         }
-
        
         if (balance < -1 && getBalance(root.right) <= 0)
             return leftRotate(root);
-
        
         if (balance < -1 && getBalance(root.right) > 0) {
             root.right = rightRotate(root.right);
@@ -189,7 +158,6 @@ public class AVLTree {
     }
 
     public void print(Node root) {
-
         if(root == null) {
             System.out.println("(XXXXXX)");
             return;
@@ -210,28 +178,19 @@ public class AVLTree {
         for(int i = 0; i < maxHalfLength*width; i++)
             sb.append(' ');
         String textBuffer;
-
        
         for(int i = 0; i < height; i++) {
-
-            sb.setLength(maxHalfLength * ((int)Math.pow(2, height-1-i) - 1));
-
-           
-            textBuffer = sb.toString();
-
-           
+            sb.setLength(maxHalfLength * ((int)Math.pow(2, height-1-i) - 1));           
+            textBuffer = sb.toString();           
             for(Node n : current) {
-
                 System.out.print(textBuffer);
 
                 if(n == null) {
-
                     System.out.print("        ");
                     next.add(null);
                     next.add(null);
 
                 } else {
-
                     System.out.printf("(%6d)", n.value);
                     next.add(n.left);
                     next.add(n.right);
@@ -242,14 +201,10 @@ public class AVLTree {
 
             }
 
-            System.out.println();
-           
+            System.out.println();           
             if(i < height - 1) {
-
                 for(Node n : current) {
-
                     System.out.print(textBuffer);
-
                     if(n == null)
                         System.out.print("        ");
                     else
@@ -263,8 +218,7 @@ public class AVLTree {
                 System.out.println();
 
             }
-
-            
+           
             elements *= 2;
             current = next;
             next = new ArrayList<Node>(elements);
